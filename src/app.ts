b8043c3f1393ffa3094083692c74863c2f0bd3f1
Francis Gauthier <fgauthier@crc-lennox.qc.ca>
2021-11-13 18:02:07 -0500

    Add cors config

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,12 @@ const Koa = require('koa');
 const Router = require('@koa/router');
 const auth = require('koa-basic-auth');
 const bodyParser = require('koa-bodyparser');
+const cors = require('koa-cors');
 // const auth = require('koa-basic-auth');
 const app = new Koa();
 // const router = new Router();
+
+// app.use(cors());
 
 // bodyparser
 app.use(bodyParser())
